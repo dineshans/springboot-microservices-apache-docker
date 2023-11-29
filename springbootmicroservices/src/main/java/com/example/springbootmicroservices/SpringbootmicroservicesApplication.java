a4040ef50947aa6e75e62184aa9d7cf022251b00
Dinesh Sahu <dineshsahuans@gmail.com>
2023-11-29 23:00:53 +0530

    added new repo

---
+++ springbootmicroservices/src/main/java/com/example/springbootmicroservices/SpringbootmicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootmicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootmicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootmicroservicesApplication.class, args);
+	}
+
+}
