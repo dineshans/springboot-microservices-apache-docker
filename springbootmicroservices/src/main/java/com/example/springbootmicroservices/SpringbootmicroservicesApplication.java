86d4a8ee6ad3f4acd65dadce6860513dec28e1a1
dineshans <dineshsahuans@gmail.com>
2023-11-29 23:20:56 +0530

    chrcks

--- springbootmicroservices/src/main/java/com/example/springbootmicroservices/SpringbootmicroservicesApplication.java
+++ springbootmicroservices/src/main/java/com/example/springbootmicroservices/SpringbootmicroservicesApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootmicroservicesApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringbootmicroservicesApplication.class, args);
+		System.out.println("Hello World!");
 	}
 
 }
